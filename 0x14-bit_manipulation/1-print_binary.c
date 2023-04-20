d155d180be069538cf68d444efc5a5e6d52c1c56
Elliken <danielkenelliot@gmail.com>
2023-04-19 17:04:37 -0700

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bitValue;
+	int bit_val;
 
 	if (index > 63)
 		return (-1);
 
-	bitValue = (n >> index) & 1;
+	bit_val = (n >> index) & 1;
 
-	return (bitValue);
+	return (bit_val);
 }
