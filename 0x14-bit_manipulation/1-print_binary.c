360a36ebbc994b0ce5cdc6492cf0de7dec7340b5
Elliken <danielkenelliot@gmail.com>
2023-07-26 16:56:28 -0700

    task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int p, count = 0;
+	unsigned long int latest;
+
+	for (p = 63; p >= 0; p--)
+	{
+		latest = n >> p;
+
+		if (latest & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
---
+++ 0x14-bit_manipulation/Readme.md
@@ -0,0 +1,1 @@
+For Bit manipulation task
